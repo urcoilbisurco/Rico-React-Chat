8eb0fadb199d4420123a79b6b41989121b1a1e7c
Francesco Marassi <urcoilbisurco@gmail.com>
2017-03-03 10:10:27 +0100

    add shouldComponentUpdate to message.js

--- src/components/messageList/message/message.js
+++ src/components/messageList/message/message.js
@@ -4,6 +4,13 @@ var User=require("./user/user");
 var css=require("./message.scss");
 
 const Message = React.createClass({
+  shouldComponentUpdate:function(nextProps, nextState){
+    //if the component ended the enter animation, it will not change anymore, so return false to stop updating.
+    if(nextProps.style.opacity==1){
+      return false;
+    }
+    return true;
+  },
   render:function() {
     return (
       <div style={this.props.style} className={cn(css.message, (this.props.isUser ? css.isOwner : ""))}>
